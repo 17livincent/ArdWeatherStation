79cc76207be9e1f37f05ba5a25fca08d4af45644
Vincent Li <li.vincent0@gmail.com>
2023-02-19 14:59:00 -0800

    Created client sketches

---
+++ client_display/client_display.ino
@@ -0,0 +1,5 @@
+/**
+ * @file client_display.ino
+ * @author Vincent Li (li.vincent0@gmail.com)
+ * @brief Code for the display client.
+ */
---
+++ client_sensor/client_sensor.ino
@@ -0,0 +1,5 @@
+/**
+ * @file client_sensor.ino
+ * @author Vincent Li (li.vincent0@gmail.com)
+ * @brief Code for the sensor client.
+ */
