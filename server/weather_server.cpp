54c9fba0917cad5685d60cfa8fea0929daeb6e8a
Vincent Li <li.vincent0@gmail.com>
2023-02-20 17:02:45 -0800

    Attach server library to weather_server

---
+++ server/weather_server.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file weather_server.cpp
+ * @author Vincent Li (li.vincent0@gmail.com)
+ * @brief Defines the executable for the server module.
+ */
+
+#include "weather_server.h"
+#include "socket_server.h"
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
---
+++ server/weather_server.h
@@ -0,0 +1,5 @@
+/**
+ * @file weather_server.h
+ * @author Vincent Li (li.vincent0@gmail.com)
+ * @brief Defines the server module for the weather station.
+ */
